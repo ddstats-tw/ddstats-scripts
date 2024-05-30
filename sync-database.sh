bd758c125b43e3defdc860bf0b10cc052632710d
furo <furo@posteo.net>
2024-05-30 21:32:33 +0200

    Read .env in shell scripts

--- sync-database.sh
+++ sync-database.sh
@@ -1,4 +1,25 @@
-wget http://ddnet.org/players.msgpack -O ../web/players.msgpack
+#!/bin/sh
+read_env() {
+  local filePath="${1:-.env}"
+
+  if [ ! -f "$filePath" ]; then
+    echo "missing ${filePath}"
+    exit 1
+  fi
+
+  while read -r LINE; do
+    # Remove leading and trailing whitespaces, and carriage return
+    CLEANED_LINE=$(echo "$LINE" | awk '{$1=$1};1' | tr -d '\r')
+
+    if [[ $CLEANED_LINE != '#'* ]] && [[ $CLEANED_LINE == *'='* ]]; then
+      export "$CLEANED_LINE"
+    fi
+  done < "$filePath"
+}
+
+read_env
+
+wget http://ddnet.org/players.msgpack -O $DDSTATS_WEB_PATH/players.msgpack
 cd data
 rm -rf ddnet*
 wget https://ddnet.org/stats/ddnet.sqlite.zip
@@ -21,10 +42,10 @@ sqlite3 -header -csv ddnet.sqlite ".output race.csv" " \
     printf('%.2f', cp25) FROM race ORDER BY map, name, time, timestamp, server"
 
 # postgres to csv
-PGPASSWORD=ddstats psql -U ddstats -h 127.0.0.1 -c "\COPY (SELECT * FROM maps ORDER BY map COLLATE \"C\") TO 'maps-psql.csv' CSV HEADER"
-PGPASSWORD=ddstats psql -U ddstats -h 127.0.0.1 -c "\COPY (SELECT map, width, height, death::int, through::int, jump::int, dfreeze::int, ehook_start::int, hit_end::int, solo_start::int, tele_gun::int, tele_grenade::int, tele_laser::int, npc_start::int, super_start::int, jetpack_start::int, walljump::int, nph_start::int, weapon_shotgun::int, weapon_grenade::int, powerup_ninja::int, weapon_rifle::int, laser_stop::int, crazy_shotgun::int, dragger::int, door::int, switch_timed::int, switch::int, stop::int, through_all::int, tune::int, oldlaser::int, teleinevil::int, telein::int, telecheck::int, teleinweapon::int, teleinhook::int, checkpoint_first::int, bonus::int, boost::int, plasmaf::int, plasmae::int, plasmau::int FROM mapinfo ORDER BY map COLLATE \"C\") TO 'mapinfo-psql.csv' CSV HEADER"
-PGPASSWORD=ddstats psql -U ddstats -h 127.0.0.1 -c "\COPY (SELECT map, name, ROUND(time::numeric, 2) as time, timestamp, UPPER(encode(id::bytea, 'hex')) as hex FROM teamrace ORDER BY map COLLATE \"C\", name COLLATE \"C\", time, timestamp, id) TO 'teamrace-psql.csv' CSV HEADER"
-PGPASSWORD=ddstats psql -U ddstats -h 127.0.0.1 -c "\COPY (SELECT map, name, ROUND(time::numeric, 2), timestamp, server, \
+psql $DATABASE_URL -c "\COPY (SELECT * FROM maps ORDER BY map COLLATE \"C\") TO 'maps-psql.csv' CSV HEADER"
+psql $DATABASE_URL -c "\COPY (SELECT map, width, height, death::int, through::int, jump::int, dfreeze::int, ehook_start::int, hit_end::int, solo_start::int, tele_gun::int, tele_grenade::int, tele_laser::int, npc_start::int, super_start::int, jetpack_start::int, walljump::int, nph_start::int, weapon_shotgun::int, weapon_grenade::int, powerup_ninja::int, weapon_rifle::int, laser_stop::int, crazy_shotgun::int, dragger::int, door::int, switch_timed::int, switch::int, stop::int, through_all::int, tune::int, oldlaser::int, teleinevil::int, telein::int, telecheck::int, teleinweapon::int, teleinhook::int, checkpoint_first::int, bonus::int, boost::int, plasmaf::int, plasmae::int, plasmau::int FROM mapinfo ORDER BY map COLLATE \"C\") TO 'mapinfo-psql.csv' CSV HEADER"
+psql $DATABASE_URL -c "\COPY (SELECT map, name, ROUND(time::numeric, 2) as time, timestamp, UPPER(encode(id::bytea, 'hex')) as hex FROM teamrace ORDER BY map COLLATE \"C\", name COLLATE \"C\", time, timestamp, id) TO 'teamrace-psql.csv' CSV HEADER"
+psql $DATABASE_URL -c "\COPY (SELECT map, name, ROUND(time::numeric, 2), timestamp, server, \
     ROUND(cp1::numeric, 2), ROUND(cp2::numeric, 2), ROUND(cp3::numeric, 2), \
     ROUND(cp4::numeric, 2), ROUND(cp5::numeric, 2), ROUND(cp6::numeric, 2), ROUND(cp7::numeric, 2), \
     ROUND(cp8::numeric, 2), ROUND(cp9::numeric, 2), ROUND(cp10::numeric, 2), ROUND(cp11::numeric, 2), \
