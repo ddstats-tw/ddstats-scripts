0e32d8044122ff345bc31f30a2cb5d69420a2e1b
furo <furo@posteo.net>
2025-03-19 22:44:31 +0100

    Add script to download map thumbnails

---
+++ misc/map-thumbnails.sh
@@ -0,0 +1,8 @@
+mkdir -p ../data/thumbnails/
+cd ../data/thumbnails/
+curl https://ddnet.org/releases/maps.json | jq -r '.[]|[.name, .thumbnail] | @tsv' |
+  while IFS=$'\t' read -r name thumbnail; do
+    if [ ! -f "$name".png ]; then
+        wget -O "$name".png "$thumbnail"
+    fi
+done
