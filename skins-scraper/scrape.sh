6da14da9c8d4c5547e9433e6e7f4a1cc6055e3f5
furo <furo@posteo.net>
2025-04-13 21:08:05 +0200

    Prevent more invalid skins

--- skins-scraper/scrape.sh
+++ skins-scraper/scrape.sh
@@ -19,13 +19,14 @@ cp -n data/teeworldsdb/06/*.png output/original/ 2>/dev/null
 # Remove any skins that don't have a alpha channel
 cd ./output/original
 identify -format '%A - %f\n' *.png 2>/dev/null | grep 'Undefined' | cut -c 13- | tr '\n' '\0' | xargs -0 rm
+identify -format '%[channels]-%f\n' *.png | grep -v 'rgba' | cut -d "-" -f2- | tr '\n' '\0' | xargs -0 rm
+
+# Remove non-matching width and height
+identify -format '%[fx:w/h]-%f\n' *.png | grep -v "^2" | cut -d "-" -f2- | tr '\n' '\0' | xargs -0 rm
 
 # Resize all skins to 512x256
 find . ../ -maxdepth 1 -regex ".*\.png" -type f -printf "%f\n" | sort | uniq -u | tr '\n' '\0' | xargs -n 2 -P 6 -0 -I {} magick "{}" -resize 512x256\> -colorspace sRGB PNG00:"../{}"
 cd ..
-
-# Remove non-matching width and height
-identify -format '%[fx:w/h]-%f\n' *.png | grep -v "2-" | cut -d "-" -f2- | tr '\n' '\0' | xargs -0 rm
 
 # Create a zip of all skins
 mkdir zips/
